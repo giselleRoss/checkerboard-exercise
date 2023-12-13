01728d20c5261e7d3290df017bbd78213f6d1df9
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2023-12-13 18:28:11 -0500

    Gradient checkerboard complete

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const container = document.createElement("div");
-document.body.appendChild(container);
+
 
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
 container.style.width = "800px";
 container.style.height = "800px";
+
 container.style.border = "5px solid black";
+container.style.backgroundImage = "linear-gradient(to bottom right, green, blue)"
 
-const randomHexColorCode = () => {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6)
-}
+document.body.appendChild(container);
 
-for(var i = 0; i <= 64; i++){
+for(let i = 0; i < 64; i++){
     const square = document.createElement("div");
     square.style.height = "12.5%";
     square.style.width = "12.5%";  
-    if(i + (Math.floor(i / 8)) % 2 === 0){
-      square.style.background = randomHexColorCode();  
+    if ((i + Math.floor(i / 8)) % 2 === 0) {
+        square.style.background = `rgba(75, 275, 100, ${i * (1/64)})`;
     } else {
-      square.style.background = randomHexColorCode();  
-    }  
+        square.style.background = `rgba(75, 30, 250, ${i * (1/64)})`;
+    }
     container.append(square);
 }
