85aa56be90663306d7d6788eafd38bf3ec562e21
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2023-12-13 16:27:59 -0500

    Random completed

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ container.style.width = "800px";
 container.style.height = "800px";
 container.style.border = "5px solid black";
 
+const randomHexColorCode = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6)
+}
+
 for(var i = 0; i <= 64; i++){
     const square = document.createElement("div");
     square.style.height = "12.5%";
     square.style.width = "12.5%";  
-
-    if((i + Math.floor(i / 8)) % 2 === 0){
-      square.style.background = "red";  
+    if(i + (Math.floor(i / 8)) % 2 === 0){
+      square.style.background = randomHexColorCode();  
     } else {
-      square.style.background = "black";  
+      square.style.background = randomHexColorCode();  
     }  
     container.append(square);
 }
